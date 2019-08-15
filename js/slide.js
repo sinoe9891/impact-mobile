544fdf0c7adc323c34406eb69e19a48c538c5192
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2019-08-15 15:31:10 -0600

    nuevas secciones

---
+++ js/slide.js
@@ -0,0 +1,69 @@
+$(document).on('ready', function() {
+    $(".vertical-center-4").slick({
+      dots: true,
+      vertical: true,
+      centerMode: true,
+      slidesToShow: 4,
+      slidesToScroll: 2
+    });
+    $(".vertical-center-3").slick({
+      dots: true,
+      vertical: true,
+      centerMode: true,
+      // slidesToShow: 3,
+      // slidesToScroll: 3
+    });
+    $(".vertical-center-2").slick({
+      dots: true,
+      vertical: true,
+      centerMode: true,
+      slidesToShow: 2,
+      slidesToScroll: 2
+    });
+    $(".vertical-center").slick({
+      dots: true,
+      vertical: true,
+      centerMode: true,
+    });
+    $(".vertical").slick({
+      dots: true,
+      vertical: true,
+      // slidesToShow: 3,
+      // slidesToScroll: 3
+    });
+    $(".regular").slick({
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      infinite: true,
+      // slidesToShow: 3,
+      // slidesToScroll: 3
+    });
+    $(".center").slick({
+      dots: true,
+      infinite: true,
+      centerMode: true,
+      slidesToShow: 5,
+      slidesToScroll: 3
+      
+    });
+    $(".variable").slick({
+      dots: true,
+      infinite: true,
+      variableWidth: true,
+      centerMode: true,
+      
+    });
+    $(".aliados").slick({
+      dots: true,
+      infinite: true,
+      variableWidth: true,
+      centerMode: true,
+      
+    });
+    $(".lazy").slick({
+      lazyLoad: 'ondemand', // ondemand progressive anticipated
+      infinite: true
+    });
+  });
+
