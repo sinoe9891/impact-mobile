7e06e3465af2dceadc2d61cd0ac95ca8652f2d24
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2019-08-23 13:31:41 -0600

    video popup swiper

--- js/slick.js
+++ js/slick.js
@@ -46,7 +46,7 @@
                 asNavFor: null,
                 // prevArrow: '<button class="slick-prev" aria-label="Previous" type="button">Previous</button>',
                 // nextArrow: '<button class="slick-next" aria-label="Next" type="button">Next</button>',
-                // autoplay: true,
+                autoplay: true,
                 autoplaySpeed: 3000,
                 centerMode: true,
                 centerPadding: '0px',
